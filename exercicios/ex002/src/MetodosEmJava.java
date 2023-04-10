cace0ef8f9e2a36af7f62619ae9a73ac287e84c4
Vinicius de Souza <viniciusdossantos547@gmail.com>
2023-04-10 17:57:35 -0300

    Criando metodos em Java

---
+++ exercicios/ex002/src/MetodosEmJava.java
@@ -0,0 +1,12 @@
+public class MetodosEmJava {
+    public static void main(String[] args) {
+        soma(5,10);
+        soma(7,17);
+        soma(8,14);
+        soma(6,18);
+        soma(8,12);
+    }
+    static void soma(int x, int y) {
+        System.out.println(x + " + " + y + " = " + (x + y));
+    }
+}
