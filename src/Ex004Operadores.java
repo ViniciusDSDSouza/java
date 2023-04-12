f6c77dfe0f23ce7517b41680cbfd4f90a4b5ad90
Vinicius de Souza <viniciusdossantos547@gmail.com>
2023-04-12 14:55:20 -0300

    Otimizando e adicionando novas funcionalidades
    Transformando as operações em métodos e permitindo o usuário digitar os dados

--- src/Ex004Operadores.java
+++ src/Ex004Operadores.java
@@ -1,26 +1,77 @@
+import java.util.Scanner;
+
 public class Ex004Operadores {
     public static void main(String[] args) {
 
-        double a = 5;
-        double b = 10;
-        String txt = a + " operador " + b + " = ";
+        double n1,n2;
+        Scanner scan = new Scanner(System.in);
 
-        double soma = a + b;
-        double subtracao = a - b;
-        double divisao = a / b;
-        double multiplicacao = a * b;
+        System.out.println("Qual o seu nome?");
+        String nome = scan.nextLine();
 
-        System.out.println(txt.replace("operador","+") + soma);
+        System.out.println("Escolha um número: ");
+        n1 = scan.nextDouble();
 
-        System.out.println(txt.replace("operador", "-") + subtracao);
+        System.out.println("Escolha outro número: ");
+        n2 = scan.nextDouble();
 
-        System.out.println(txt.replace("operador","/") + divisao);
+        mensagem(nome);
 
-        System.out.println(txt.replace("operador","*") + multiplicacao);
+        soma(n1,n2);
+        subtracao(n1, n2);
+        multiplicacao(n1, n2);
+        divisao(n1, n2);
 
-        String frase = "Olá nome, tudo bem com você?";
-        String nome = "Vinicius";
-        System.out.println("\n" + frase);
-        System.out.println(frase.replace("nome",nome));
+        scan.close();
+    }
+
+    static void soma(double  n1, double n2) {
+        double somaRes = n1 + n2;
+        String txt = n1 + " + " + n2 + " = ";
+
+        // Retirar .00 dos números inteiros
+        if (somaRes - (int)somaRes != 0) {
+            System.out.println(txt + somaRes);
+        } else if (somaRes - (int)somaRes == 0) {
+            System.out.println(txt + (int)somaRes);
+        }
+    }
+
+    static void subtracao(double n1, double n2){
+        double subtracaoRes = n1 - n2;
+        String txt = n1 + " - " + n2 + " = ";
+
+        if (subtracaoRes - (int)subtracaoRes != 0) {
+            System.out.println(txt + subtracaoRes);
+        } else if (subtracaoRes - (int)subtracaoRes == 0) {
+            System.out.println(txt + (int)subtracaoRes);
+        }
+    }
+
+    static void multiplicacao (double n1, double n2) {
+        double multiplicacaoRes = n1 * n2;
+        String txt = n1 + " * " + n2 + " = ";
+        
+        if (multiplicacaoRes - (int)multiplicacaoRes != 0) {
+            System.out.println(txt + multiplicacaoRes);
+        } else if (multiplicacaoRes - (int)multiplicacaoRes == 0) {
+            System.out.println(txt + (int)multiplicacaoRes);
+        }
+    }
+
+    static void divisao(double n1, double n2) {
+        double divisaoRes = n1 / n2;
+        String txt = n1 + " / " + n2 + " = ";
+        
+        if (divisaoRes - (int)divisaoRes != 0) {
+            System.out.println(txt + divisaoRes);
+        } else if (divisaoRes - (int)divisaoRes == 0) {
+            System.out.println(txt + (int)divisaoRes);
+        }
+    }
+
+    static void mensagem(String nome){
+        String frase = "Olá [nome], aqui estão os resultados:\n";
+        System.out.println(frase.replace("[nome]", nome));
     }
 }
