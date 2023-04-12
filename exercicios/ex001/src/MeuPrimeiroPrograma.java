1d875b94aa810043ec7d6b2985aa62ba47b2e7ac
Vinicius de Souza <viniciusdossantos547@gmail.com>
2023-04-12 08:19:53 -0300

    Retirando comentários e definindo variável na classe

--- exercicios/ex001/src/MeuPrimeiroPrograma.java
+++ exercicios/ex001/src/MeuPrimeiroPrograma.java
@@ -1,30 +1,24 @@
 public class MeuPrimeiroPrograma {
+    public static String nome = "Vinicius";
     public static void main(String[] args) {
-
-        // Definindo Variáveis
-        String nome = "Vinicius";
         int idade = 17;
         double dinheiro = 7.45;
         char generoAbreviado = 'm';
         String genero = null;
         boolean maioridade;
 
-        // Maior ou Menor de idade
         if (idade >= 18) {
             maioridade = true;
         } else {
             maioridade = false;
         }
 
-
-        // Definindo o gênero
         if (generoAbreviado == 'm') {
             genero = "Masculino";
         } else if (generoAbreviado == 'f') {
             genero = "Feminino";
         }
 
-        // Imprimindo resultados
         System.out.println("Olá " + nome + "!");
         System.out.println("Você possui " + idade + " anos de idade!");
 
