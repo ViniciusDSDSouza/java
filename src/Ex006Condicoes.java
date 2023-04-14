d7654fbca42dcd37588b859c38fcb0f2e83fe86e
Vinicius de Souza <viniciusdossantos547@gmail.com>
2023-04-14 15:42:00 -0300

    Criando condições
    E deletando classes inúteis

---
+++ src/Ex006Condicoes.java
@@ -0,0 +1,14 @@
+public class Ex006Condicoes {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        if (x > y){
+            System.out.println("x é maior que y");
+        } else if (x < y){
+            System.out.println("x é menor que y");
+        }else {
+            System.out.println("x é = a y");
+        }
+    }
+}
