46541cad9d6f09ddc1e31310a4d3bdff0319e930
Vinicius de Souza <viniciusdossantos547@gmail.com>
2023-04-17 23:28:44 -0300

    Arrumando o titulo

--- src/Ex007InterfaceComJavaSwing/Ex007InterfaceComJavaSwing.java
+++ src/Ex007InterfaceComJavaSwing/Ex007InterfaceComJavaSwing.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class Ex007InterfaceComJavaSwing {
     public static void main(String[] args) {
         
-        String n1txt = JOptionPane.showInputDialog(null,"Digite um número: ",null, JOptionPane.QUESTION_MESSAGE);
+        String n1txt = JOptionPane.showInputDialog(null,"Digite um número: ","Somando Valores", JOptionPane.QUESTION_MESSAGE);
         
         String n2txt = JOptionPane.showInputDialog(null, "Digite outro número: ", "Somando Valores", JOptionPane.QUESTION_MESSAGE);
 
@@ -14,7 +14,5 @@ public class Ex007InterfaceComJavaSwing {
         int soma = n1+n2;
 
         JOptionPane.showMessageDialog(null, "Resultado = " + soma);
-
-        System.out.println(soma);
     }
 }
